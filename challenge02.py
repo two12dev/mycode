40fa0a0824c9972909df5c9228e0bec5c93ac462
Clinton Robertson <crob2099@gmail.com>
2021-06-08 15:14:30 +0000

    learning about dictionaries

---
+++ challenge02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+icecream = ["flavors", "salty"] 
+tlgclass = ["Brian","Clint","Damian","Dan","David","Jelani","Jerad","Jon","Jun","Mark","Max"]
+
+icecream.append(99)
+random_num = int(input("Pick a number from 0 - 10:\n"))
+print(f"{icecream[-1]} {icecream[0]}, and {tlgclass[random_num]} chooses to be {icecream[1]}")
+
+
